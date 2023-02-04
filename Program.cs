0ce3b0cf46a802ec8154d3053cff0b97899c0238
User <User@DESKTOP-EVDBN8V>
2023-02-05 02:52:57 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int count = (int)Math.Log10(number) + 1;
+            int r;
+            int sumOfNumber = 0;
+            for (int i = count; i > 0; i--)
+            {
+                r = number % 10;
+                number /= 10;
+                sumOfNumber += r * i;
+            }
+            Console.WriteLine(sumOfNumber);
+
+
+        }
+        
+    }
+}
